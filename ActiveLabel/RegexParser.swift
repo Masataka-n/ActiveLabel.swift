589c81ad54598a8c4e2817e004fe2f6ac0033ceb
Masataka Nakagawa <n.masataka.hal.ih@gmail.com>
2020-10-21 18:20:39 +0900

    Fix NSRegularExpression options

--- ActiveLabel/RegexParser.swift
+++ ActiveLabel/RegexParser.swift
@@ -26,7 +26,7 @@ struct RegexParser {
     private static func regularExpression(for pattern: String) -> NSRegularExpression? {
         if let regex = cachedRegularExpressions[pattern] {
             return regex
-        } else if let createdRegex = try? NSRegularExpression(pattern: pattern, options: [.caseInsensitive]) {
+        } else if let createdRegex = try? NSRegularExpression(pattern: pattern, options: [.anchorsMatchLines]) {
             cachedRegularExpressions[pattern] = createdRegex
             return createdRegex
         } else {
